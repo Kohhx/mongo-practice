88d2f23ea8786302ccc1b1db6280ad4b1174b190
He Xiang Koh <kohhxx6@gmail.com>
2023-01-25 00:07:24 +0800

    Completed finding one  user and test

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -13,4 +13,14 @@ describe("Reading User out of database", () => {
     const users = await User.find({name: "Joe"})
     assert(users[0]._id.toString === joe._id.toString)
   })
+
+  it("find one user with particular ID using findOne", async() => {
+    const users = await User.findOne({_id: joe._id});
+    assert(users.name=== "Joe")
+  })
+
+  it("find one user with particular ID using findbyID", async() => {
+    const users = await User.findById(joe._id);
+    assert(users.name === "Joe")
+  })
 })
