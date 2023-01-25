43cb5249bd1511dc903355532abf8e4c6870cd79
He Xiang Koh <kohhxx6@gmail.com>
2023-01-25 16:46:42 +0800

    Add subdocument and create test

---
+++ users/src/post.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: String,
+});
+
+module.exports = postSchema;
---
+++ users/test/subdocument_test.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Sub Documents", () => {
+  it("can create a subdocument", async () => {
+    const joe = new User({
+      name: "Joe",
+      posts: [{ title: "post1" }],
+    });
+
+    await joe.save();
+    const user = await User.findOne({name: "Joe"})
+    assert(user.posts[0].title === "post1")
+  });
+
+  it("can add subdocuments to an existing record", async () => {
+    const joe = new User({
+      name: "Joe",
+      posts: [],
+    });
+
+    await joe.save();
+
+    let user;
+    user = await User.findOne({name: "Joe"})
+    user.posts.push({title: 'New Post'})
+    await user.save();
+    user = await User.findOne({name: "Joe"})
+    assert(user.posts[0].title === "New Post")
+  });
+});
